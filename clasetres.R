0646be07a4224c0dc3b3abec9f0559dd6e9bb8e8
lisscata <lissette.umanap@utem.cl>
2020-05-23 19:39:59 -0400

    CLASE3

---
+++ clasetres.R
@@ -0,0 +1,171 @@
+if(TRUE) {
+  2+2
+}
+2 < 4  
+
+
+
+##
+if(2 < 4) {
+  2+2
+}
+ 
+if(FALSE) {
+  2+2
+}
+
+##
+ 
+if(2 > 4) {
+  
+  2+2
+}
+########################
+
+var1 <- 1 
+var1 
+
+
+if(2<4) {
+  var1 <- 2 + 2 
+
+}
+var1 
+
+##############
+
+var1 <- 2 
+var1 == 2 
+
+if(var1 == 2 ) {
+  var1 <- var1 + 2 
+  
+}
+var1
+
+#############
+
+
+variable1 <- 0 
+
+
+if( variable1 == 0 ){
+  variable1 <- variable1 + 1
+}
+
+rm(variable1)
+
+##########
+
+var1 <- 5 
+var1 %% 2 == 0
+
+if(var1 %% 2 == 0){
+  var1 <- var1 + 1 
+  
+}
+var1 
+
+###
+
+varX <- 5
+varX 
+
+if(varX %% 2 == 0){
+  print("El numero es par")
+  varX * 2
+} else { 
+  print("el numero es impar")
+  varX * 3
+}
+
+print(varX) 
+
+varX * 3
+
+######
+
+x <- 10
+
+if(x <= 0){
+  print("x es menor igual a cero")
+  x <- 1
+  print(x)
+} else {
+  print("x es mayor igual a cero")
+  x <- 2 * x^2
+  print(x)
+}
+
+###
+
+ y <- 3
+ 
+ if(y == 3){
+   print("y vale tres")
+ } else if( y < 3) {
+   print("y es menor a tres")
+   
+ } else {
+   print("y es mayor a tres")
+ }
+
+
+######
+ 
+# si x es menor a 0, la respuesta es 1
+ # si x es mayor a 0 se mantiene 
+ # si x = 0 entonces la respuesta es 5 
+ 
+ 
+
+ x <- 5
+ 
+ if(x > 0){
+   print("x es mayor igual a cero")
+   x <- 2 * x^2
+   print(x)
+   
+ } else if (x < 0) {
+   print("x es menor igual a cero")
+   x <- 1
+   print(x)
+ } else {
+   print("x es cero")
+   x <- 5
+   print("x")
+ }
+## LISSETTE
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
